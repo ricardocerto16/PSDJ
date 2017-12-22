fcae6d05bec608b9b9c317acaa4a056750ad3968
João Cabo <Ninjadinosaur.ilikecake@gmail.com>
2017-12-22 18:52:51 +0000

    limpando lixo

--- src/business/RelogioUser.java
+++ src/business/RelogioUser.java
@@ -57,38 +57,5 @@ public class RelogioUser implements Relogio {
         String data = ld.format(ofPattern("dd MMMM yyyy"));
         return diasemana + ", " + data;
     }
-    
-    
-    /*
-    public String getDiaSemana() {
-        String diasemana = String.valueOf(LocalDateTime.now().getDayOfWeek());
-        return diasemana;
-    }
-
-    public String getDia() {
-        String diasemana = String.valueOf(LocalDateTime.now().getDayOfMonth());
-        return diasemana;
-    }
-
-    public String getMes() {
-        String diasemana = String.valueOf(LocalDateTime.now().getMonth());
-        return diasemana;
-    }
-
-    public String getAno() {
-        String ano = String.valueOf(LocalDateTime.now().getYear());
-        return ano;
-    }
-
-    public String getHora() {
-        String hora = String.valueOf(LocalDateTime.now().getHour());
-        return hora;
-    }
-
-    public String getMinuto() {
-        String minuto = String.valueOf(LocalDateTime.now().getMinute());
-        return minuto;
-    }
-    */
 }
 
