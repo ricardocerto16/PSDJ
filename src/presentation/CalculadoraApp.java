a5476baaf0d93a76e187940a1a0eb7ed8a725232
ricardocerto16 <a75315@alunos.uminho.pt>
2017-12-24 13:45:13 +0000

    algumas correções no menu

--- src/presentation/CalculadoraApp.java
+++ src/presentation/CalculadoraApp.java
@@ -169,7 +169,7 @@ public class CalculadoraApp {
 
         String [] fusosh = {
                 "                                       * *                                                                                       * *",
-                "                                       * *                            1 - DIFRENÇA DE FUSOS ENTRE 2 ZONAS                        * *",
+                "                                       * *                            1 - DURAÇÃO DE UMA VIAGEM ENTRE 2 ZONAS                        * *",
                 "                                       * *                                                                                       * *",
                 "                                       * *                            2 - TEMPO ATUAL NUMA ZONA                                  * *",
                 "                                       * *                                                                                       * *",
@@ -1043,76 +1043,7 @@ public class CalculadoraApp {
             System.out.println("Pressione Enter Para Continuar");
         }
     }
-
-
-
-/*
-    private static void calculadoraDataHoraEm() {
-        //dado um pais, dizer a data e hora nesse país
-
-        String resposta,pais, pais_resposta;
-        Set<String>  zones = ZoneId.getAvailableZoneIds();
-
-
-
-        clearScreen();
-        System.out.println(                                "                                       * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-        System.out.println(                                "                                       * *                                                                                       * *");
-
-        pais = startMenu.readString(                  "                                       * *       Introduza o país para o qual deseja saber a data e hora locais:                 * *");
-
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(pais);
-        java.util.regex.Matcher m = p.matcher(", ");
-        ArrayList<String> als = new ArrayList<>();
-        for(String s : zones)
-        {
-            m.reset(s);
-            if(m.find()){
-                als.add(s);
-                System.out.println(s);
-            }
-        }
-        while(als.isEmpty()){
-            pais = startMenu.readString(              "                                       * *       Este País Não Existe, Insira Novamente:                                         * *");
-
-            java.util.regex.Pattern pp = java.util.regex.Pattern.compile(pais);
-            java.util.regex.Matcher mm = pp.matcher(", ");
-            ArrayList<String> alsp = new ArrayList<>();
-            for(String s : zones)
-            {
-                mm.reset(s);
-                if(mm.find()){
-                    alsp.add(s);
-                    System.out.println(s);
-                }
-            }
-            als = alsp;
-        }
-
-
-        pais_resposta = startMenu.readString(         "                                       * *       Entre Esta Lista de Países Apresentada, Escolha Um:                             * *");
-        while(!als.contains(pais_resposta)){
-            System.out.println("\n> O Valor Introduzido Não É Válido!\n");
-            pais_resposta = startMenu.readString(     "                                       * *       Entre Esta Lista de Países Apresentada, Escolha Um:                             * *");
-        }
-
-        System.out.println("> Valor Introduzido!\n");
-        System.out.println(                                "                                       * *                         O País Foi Introduzido Com Sucesso                            * *");
-        System.out.println(                                "                                       * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-        System.out.println(                                "                                                       * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *              \n");
-
-
-        resposta = principal.dataHoraEm(pais_resposta);
-        System.out.println(resposta);
-
-
-        System.out.println("Pressione Enter Para Continuar");
-        while (!startMenu.readString("").equals("")) {
-            System.out.println("Pressione Enter Para Continuar");
-        }
-    }
-
-*/
+    
 
     private static void calculadoraPrimeiroDiaMes() {
         //data do primeiro dia de um dado mes (2 -> sld 31); retornar também o dia da semana
@@ -1362,7 +1293,7 @@ public class CalculadoraApp {
 
         Duration dura = fusos.duracaoViagem(date1,local1,date2,local2);
 
-        System.out.println("A diferença entre fusos é de :  " + dura);
+        System.out.println("A duração de viagem é de :  " + dura);
 
         System.out.println("Pressione Enter Para Continuar");
         while (!startMenu.readString("").equals("")) {
