364d1cc5e770950e31d0c53c0d87180d142a77f2
João Cabo <Ninjadinosaur.ilikecake@gmail.com>
2018-01-07 21:47:19 +0000

    teste 4 feito com alguns bugs de streams darem diferentes :squirrel:

--- Benchmarking/Benchmarking/src/Testes/Teste4.java
+++ Benchmarking/Benchmarking/src/Testes/Teste4.java
@@ -25,13 +25,13 @@ public class Teste4 implements ITestes {
  
     @Override
     public void execute() {
-        BiFunction<Integer,Integer,Integer> divisao = (a,b)->a/b; 
+        BiFunction<Integer,Integer,Integer> divisao = (a,b)->a!=0&&b!=0 ? (a>b ? a/b : b/a) : 1; 
         int i;
         
         for(i=0;i<4;i++){
             
             int[] array = randomArray(i);
-            int[] arrayTeste = {1,2,3,4,5,6};
+        
         /**
          * SEQUENCIAL
          */
@@ -41,14 +41,13 @@ public class Teste4 implements ITestes {
         Supplier<Integer> supplier1 = () -> Arrays.stream(array.clone()).reduce(0, (a,b)->div(a,b));
         SimpleEntry<Double,Integer> result1 = Utilidades.testeBoxGenW(supplier1);
         
-            System.out.println("PASSEI AQUI");
-        
+            
         //BIFUNCTION
         Supplier<Integer> supplier2 = () -> Arrays.stream(array.clone()).reduce(0,(a,b)->divisao.apply(a,b));
         SimpleEntry<Double,Integer> result2 = Utilidades.testeBoxGenW(supplier2);
         
         //EXPRESSÃO LAMBDA
-        Supplier<Integer> supplier3 = () -> Arrays.stream(array.clone()).reduce(0,(a,b)->a/b);
+        Supplier<Integer> supplier3 = () -> Arrays.stream(array.clone()).reduce(0,(a,b)-> a!=0&&b!=0 ? (a>b ? a/b : b/a) : 1);
         SimpleEntry<Double,Integer> result3 = Utilidades.testeBoxGenW(supplier3);
         
         /**
@@ -56,7 +55,7 @@ public class Teste4 implements ITestes {
          */
         
         //MÉTODO STATIC DIV
-        /*
+       
         Supplier<Integer> supplier4 = () -> Arrays.stream(array.clone()).parallel().reduce(0,(a,b)->div(a,b));
         SimpleEntry<Double,Integer> result4 = Utilidades.testeBoxGenW(supplier4);
             
@@ -67,10 +66,10 @@ public class Teste4 implements ITestes {
         
         
         //EXPRESSÃO LAMBDA
-        Supplier<Integer> supplier6 = () -> Arrays.stream(array.clone()).parallel().reduce(0,(a,b)->a/b);
+        Supplier<Integer> supplier6 = () -> Arrays.stream(array.clone()).parallel().reduce(0,(a,b)->a!=0&&b!=0 ? (a>b ? a/b : b/a) : 1);
         SimpleEntry<Double,Integer> result6 = Utilidades.testeBoxGenW(supplier6);
         
-        */
+        
         /**
          * IMPRESSÃO DE RESULTADOS
          */
@@ -91,16 +90,16 @@ public class Teste4 implements ITestes {
          *  STREAMS PARALELAS
          */
         
-        /*
-        System.out.println("Tempo do Método Static com Streams Sequenciais: " + result4.getKey() + 
+        
+        System.out.println("Tempo do Método Static com Streams Paralelas: " + result4.getKey() + 
                            "\n" + "Resultado da divisão: " + result4.getValue() +"\n\n");
        
-        System.out.println("Tempo de BiFunction com Streams Sequenciais: " + result5.getKey() + "\n" + 
+        System.out.println("Tempo de BiFunction com Streams Paralelas: " + result5.getKey() + "\n" + 
                            "Resultado da divisão: " + result5.getValue() +"\n\n");
        
-        System.out.println("Tempo de Expressão Lambda com Streams Sequenciais: " + result6.getKey() + "\n" + 
+        System.out.println("Tempo de Expressão Lambda com Streams Paralelas: " + result6.getKey() + "\n" + 
                            "Resultado da divisão: " + result6.getValue() +"\n\n");
-       */
+       
         
         }
         
@@ -115,7 +114,13 @@ public class Teste4 implements ITestes {
      * @return 
      */
     private static int div(int a, int b){
-        return a/b;
+        if(a!=0 && b!=0){
+            if(a>b)
+                return a/b;
+            else
+                return b/a;
+        }
+        return 1;
     }
     
     
@@ -124,24 +129,28 @@ public class Teste4 implements ITestes {
         int i;
         switch (caso) {           
             case 0:
+                System.out.println("Array com 1 Milhão de inteiros");
                 int[] lista = new int[1000000];
                 for(i=0;i<1000000;i++){
                     lista[i] = r.nextInt(9999+1)+1;
                 }
                 return lista;
             case 1:
+                System.out.println("Array com 2 Milhões de inteiros");
                 int[] lista2 = new int[2000000];
                 for(i=0;i<2000000;i++){
                     lista2[i] = r.nextInt(9999+1)+1;
                 }
                 return lista2;
             case 2:
+                System.out.println("Array com 4 Milhões de inteiros");
                 int[] lista3 = new int[4000000];
                 for(i=0;i<4000000;i++){
                     lista3[i] = r.nextInt(9999+1)+1;
                 }
                 return lista3;
             case 3:    
+                System.out.println("Array com 8 Milhões de inteiros");
                 int[] lista4 = new int[8000000];
                 for(i=0;i<8000000;i++){
                     lista4[i] = r.nextInt(9999+1)+1;
