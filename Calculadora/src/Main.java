1a5ba9aeea9327864c75cb75aa92c195dcf43978
ricardocerto16 <a75315@alunos.uminho.pt>
2018-01-11 15:29:42 +0000

    main fora dos packages

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package presentation;
+
 
 import business.Cronometro;
 import business.Fusos;
 import business.RelogioUser;
 import business.Resposta;
+import presentation.CalculadoraApp;
+
 
 /**
  *
